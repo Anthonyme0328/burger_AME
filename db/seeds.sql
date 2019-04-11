878a879b5232d6dff3d93bf0fd84967a02d374b0
Anthonyme <46856315+Anthonyme0328@users.noreply.github.com>
2019-04-10 20:24:43 -0700

    final

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,9 +1,8 @@
-INSERT INTO burgers (burger_name, devoured) VALUES ('whopper', true);
-INSERT INTO burgers (burger_name, devoured ) VALUES ('big mac', true);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Double Bacon', true);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Animal Style', true);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Baconator', true);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Quarter Pounder', true);
-
-
-
+    
+INSERT INTO burgers (burger_name, devoured) VALUES ("Big Boi Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Krabby Patty", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Veggie", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Big Mac", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Whopper", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Chicken Sandwhich", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Filet-O-fish", true);

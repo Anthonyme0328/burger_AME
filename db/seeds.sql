052367384703bb190ae64af427612c0c54276641
Anthonyme <46856315+Anthonyme0328@users.noreply.github.com>
2019-04-10 18:26:32 -0700

    ame

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('whopper', true);
+INSERT INTO burgers (burger_name, devoured ) VALUES ('big mac', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Bacon', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Animal Style', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Baconator', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Quarter Pounder', true);
+
+
+
